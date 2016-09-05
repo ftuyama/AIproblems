a866acf6fa90ae1601f2dd65a3dea7194d1bbf2d
Felipe Tuyama <ftuyama@gmail.com>
2016-09-05 11:49:53 -0300

    N-Rainhas

--- Lab2/N-Rainhas.py
+++ Lab2/N-Rainhas.py
@@ -2,9 +2,10 @@
 # !/usr/bin/env python
 """Algoritmo das N-Rainhas."""
 from graphics import *
+from random import randint
 
 
-def print_board():
+def print_board(board):
     u"""Desenha o grafo de rotas a partir do mapa lido."""
     win = GraphWin('N-Rainhas', 850, 650)
     win.setBackground(color_rgb(188, 237, 145))
@@ -12,6 +13,7 @@ def print_board():
     title.setSize(20)
     title.draw(win)
 
+    # Desenha tabuleiro principal
     rect = Rectangle(
         Point(150 - 5, 100 - 5),
         Point(650 + 5, 600 + 5)
@@ -19,6 +21,7 @@ def print_board():
     rect.setFill('brown')
     rect.draw(win)
 
+    # Desenha as casas no tabuleiro
     square = 500 / N
     for i in range(N):
         for j in range(N):
@@ -31,16 +34,85 @@ def print_board():
                 )
                 rect.setFill('gray')
                 rect.draw(win)
-                if board[i][j] == 1:
-                    cir = Circle(
-                        Point(x + 0.5 * square, y + 0.5 * square), 20
-                    )
-                    cir.setFill('blue')
-                    cir.draw(win)
+
+        # Desenha as peças no tabuleiro
+        x = 150 + i * square
+        y = 100 + board[i] * square
+        cir = Circle(
+            Point(x + 0.5 * square, y + 0.5 * square), 160 / N
+        )
+        cir.setFill('blue')
+        cir.draw(win)
 
     win.getMouse()
     win.close()
 
+
+def restricoes(board):
+    u"""Número de restrições violadas."""
+    restricoes = 0
+    for i in range(N):
+        for j in range(i):
+            if check(board, i, j):
+                restricoes += 1
+    return restricoes
+
+
+def check(board, i, j):
+    u"""Número de ataques no tabuleiro."""
+    return (board[i] == board[j]) or \
+        (abs(i - j) == abs(board[i] - board[j]))
+
+
+def max_violations(board):
+    u"""Peça que viola mais restrições."""
+    max_violations = 0
+    max_violeted = []
+    for i in range(N):
+        restricoes = 0
+        for j in range(N):
+            if i != j and check(board, i, j):
+                restricoes += 1
+        if restricoes > max_violations:
+            max_violations = restricoes
+            max_violeted = [i]
+        elif restricoes == max_violations:
+            max_violeted.append(i)
+    return max_violeted
+
+
+def hill_climbing(board, max):
+    u"""Maximiza função de requisito."""
+    for i in range(max):
+        if restricoes(board) == 0:
+            return board, i
+
+        if randint(0, 10) > 5:
+            board[randint(0, 7)] = randint(0, 7)
+        else:
+            best = 0
+            current = restricoes(board)
+            pieces = max_violations(board)
+            j = pieces[randint(0, len(pieces) - 1)]
+            for i in range(N):
+                board[j] = i
+                inspection = restricoes(board)
+                if inspection < current:
+                    best = i
+                    current = inspection
+            board[j] = best
+    return board, max
+
+
+def monte_carlo(n_squares, depth):
+    u"""Run algorithm several times."""
+    total_steps = 0
+    for i in range(depth):
+        if i % depth / 10 == 0:
+            print i
+        board = [0] * n_squares
+        total_steps += hill_climbing(board, 1000)[1]
+    print "Avarage steps: " + str((total_steps * 1.0) / depth)
 
 # Rotina main()
 print "*************************************"
@@ -49,7 +121,11 @@ print "*       Problema das N-Rainhas      *"
 print "*                                   *"
 print "*************************************"
 N = 8
-board = [[0] * N for i in range(N)]
-for i in range(N):
-    board[i][i] = 1
-print_board()
+demo = True
+if demo:
+    board = [0] * N
+    (solution, steps) = hill_climbing(board, 1000)
+    print "Solved in " + str(steps) + " steps"
+    print_board(solution)
+else:
+    monte_carlo(N, 50)
--- Lab2/funcao.py
+++ Lab2/funcao.py
@@ -2,6 +2,8 @@
 # !/usr/bin/env python
 u"""Algoritmo do Máximo de f(x)."""
 import math
+from random import randint
+from random import uniform
 
 
 def funcao(x, y):
@@ -13,6 +15,29 @@ def funcao(x, y):
     return termo1 + termo2 + termo3 + termo4
 
 
+def tempera_simulada():
+    u"""Forjando a melhor espada - máximo da função."""
+    temp = 1024.0
+    x = 10
+    y = 10
+    while True:
+        if temp < 1:
+            return (x, y)
+
+        xx = x + uniform(-0.5, 0.5)
+        yy = y + uniform(-0.5, 0.5)
+        delta = funcao(x, y) - funcao(xx, yy)
+        if delta >= 0:
+            x = xx
+            y = yy
+        else:
+            if randint(0, 100) < 100 * math.exp(delta / temp):
+                x = xx
+                y = yy
+
+        temp = temp / 2
+
+
 # Rotina main()
 print "*************************************"
 print "*                                   *"
@@ -20,11 +45,17 @@ print "*     Problema do Máximo de f(x)    *"
 print "*                                   *"
 print "*************************************"
 
-print funcao(-1, -1)
-print funcao(-1, 1)
-print funcao(0, 0)
-print funcao(1, -1)
-print funcao(1, 1)
+demo = False
+if demo:
+    print funcao(-1, -1)
+    print funcao(-1, 1)
+    print funcao(0, 0)
+    print funcao(1, -1)
+    print funcao(1, 1)
 
-print funcao(5, 0)
-print funcao(5, 5)
+    print funcao(5, 0)
+    print funcao(5, 5)
+
+solution = tempera_simulada()
+print solution
+print funcao(solution[0], solution[1])
