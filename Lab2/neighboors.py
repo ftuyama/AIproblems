2751ae7a575f9fcae6fedce6403e02b33e919a20
Felipe Tuyama <ftuyama@gmail.com>
2016-09-17 09:12:14 -0300

    Neighboor 1

--- Lab2/neighboors.py
+++ Lab2/neighboors.py
@@ -26,9 +26,14 @@ class Piece(object):
         u"""Verifica se contém a propriedade."""
         return set(dic.items()).issubset(set(self.dicty.items()))
 
-    def length(self):
+    def size(self):
         u"""Retorna tamanho do dicionário."""
         return len(self.dicty.keys())
+
+    def same(self, piece):
+        u"""Verifica se duas peças são a mesma."""
+        inteserctions = set(self.dicty.items()) & set(piece.dicty.items())
+        return len(inteserctions) == self.size()
 
 
 class Puzzle(object):
@@ -71,7 +76,7 @@ class Puzzle(object):
         if 'number' in piece1.joints() and 'number' in piece2.joints():
             return piece1.dicty['number'] - piece2.dicty['number'] == 1
 
-    def board_restriction(self, piece1, piece2):
+    def board_restriction(self, piece1, piece2, repeat):
         u"""Verifica restrições do tabuleiro."""
         if piece1.has({'cigarro': 'chesterfield'}) and \
                 piece2.has({'animal': 'raposa'}):
@@ -88,55 +93,96 @@ class Puzzle(object):
             if not self.neighboors(piece1, piece2):
                 return False
 
-        if piece1.has({'casa': 'verde'}) and \
-                piece2.has({'casa': 'marfim'}):
-            if not self.neighboors_right(piece1, piece2):
-                return False
+        if not repeat:
+            if piece1.has({'casa': 'verde'}) and \
+                    piece2.has({'casa': 'marfim'}):
+                if not self.neighboors_right(piece1, piece2):
+                    return False
 
         return True
 
     def board_fits(self, piece1, piece2):
         u"""Verifica se a peça se encaixa no tabuleiro."""
-        return self.board_restriction(piece1, piece2) and \
-            self.board_restriction(piece2, piece1)
+        return self.board_restriction(piece1, piece2, False) and \
+            self.board_restriction(piece2, piece1, True)
 
     def piece_fits(self, piece1, piece2):
         u"""Verifica se a peça se encaixa."""
-        return len(piece1.joints() & piece2.joints()) == 0
+        intersection = piece1.joints() & piece2.joints()
+        if len(intersection) == 0:
+            return True
+        for key in intersection:
+            if piece1.dicty[key] != piece2.dicty[key]:
+                return False
+        return True
 
     def fits(self, piece1, piece2):
         u"""Verifica se encaixe é possível."""
         return self.piece_fits(piece1, piece2) and \
             self.board_fits(piece1, piece2)
 
+    def remove(self, pieces, piece):
+        u"""Remove uma peça do quebra-cabeças."""
+        for a_piece in pieces:
+            if a_piece.same(piece):
+                pieces.remove(a_piece)
+                return
+
     def connect_pieces(self, pieces, piece1, piece2):
         u"""Encaixa duas peças no quebra-cabeças."""
-        pieces.remove(piece1)
-        pieces.remove(piece2)
-        pieces.append(
-            Piece(dict(piece1.dicty.items() + piece2.dicty.items()))
-        )
+        self.remove(pieces, piece1)
+        self.remove(pieces, piece2)
+        new_dicty = piece1.dicty
+        new_dicty.update(piece2.dicty)
+        pieces.append(Piece(new_dicty))
+
+    def divide(self, pieces):
+        u"""Seleciona peças para tentativas."""
+        size_pieces = [[], [], [], [], []]
+        for piece in pieces:
+            if piece.size() != 6:
+                size_pieces[piece.size() - 1].append(piece)
+        return size_pieces
 
     def select_pieces(self, pieces):
         u"""Seleciona peças para tentativas."""
+        (pieces1, pieces2, pieces3, pieces4, pieces5) = self.divide(pieces)
+        possible = []
+        if len(pieces5) > 0:
+            return (pieces5, pieces1)
+        if len(pieces4) > 0:
+            if len(pieces2) > 0:
+                return (pieces4, pieces2)
+            return (pieces4, pieces1)
+        if len(pieces3) > 1:
+            return (pieces3, pieces3)
+        if len(pieces3) > 0:
+            if len(pieces2) > 0:
+                return (pieces3, pieces2)
+            return (pieces3, pieces1)
+        if len(pieces2) > 1:
+            return (pieces2, pieces2)
+        if len(pieces2) > 0:
+            return (pieces2, pieces1)
+        return (pieces1, pieces1)
 
     def backtracking(self, pieces):
         u"""Usa backtracking para resolver."""
         if len(pieces) == 5:
             return pieces
-        for piece1 in pieces:
-            for piece2 in pieces:
-                if piece1.length() + piece2.length() > 5:
-                    pass
-                if self.fits(piece1, piece2):
-                    self.print_puzzle(pieces)
-                    new_pieces = copy.copy(pieces)
-                    self.connect_pieces(new_pieces, piece1, piece2)
-                    if not (new_pieces in visited):
-                        visited.append(pieces)
-                        result = self.backtracking(new_pieces)
-                        if result is not None:
-                            return result
+        (pieces1, pieces2) = self.select_pieces(pieces)
+        for piece1 in pieces1:
+            for piece2 in pieces2:
+                if not piece1.same(piece2):
+                    if self.fits(piece1, piece2):
+                        self.print_puzzle(pieces)
+                        new_pieces = copy.copy(pieces)
+                        self.connect_pieces(new_pieces, piece1, piece2)
+                        if not (new_pieces in visited):
+                            visited.append(pieces)
+                            result = self.backtracking(new_pieces)
+                            if result is not None:
+                                return result
         return None
 
     def solve(self):
